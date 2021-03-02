37d92ab30f88edc8cd5ecf8787c83f05e3d44f46
Contoqueren <assageant-flyff@hotmail.fr>
2021-03-02 16:10:33 +0100

    Création de l'index des models

--- back/app/models/index.js
+++ back/app/models/index.js
@@ -6,16 +6,106 @@ const Product = require('./product');
 const Review = require('./review');
 const Stock = require('./stock');
 
+// Relation NN Produit/Category
 Product.belongsToMany(Category, {
     as: "categories",
-    through: "category_product",
+    through: "product_category",
     foreignKey: "product_id",
     otherKey: "category_id"
-})
+});
 
-Product.belongsToMany(Category, {
-    as: "categories",
-    through: "category_product",
+Category.belongsToMany(Product, {
+    as: "products",
+    through: "product_category",
     foreignKey: "category_id",
     otherKey: "product_id"
-})
+});
+
+//Relation 1N Produit/Stock
+Product.hasMany(Stock, {
+    foreignKey: "product_id",
+    as: "stocks"
+});
+
+Stock.belongsTo(Product, {
+    foreignKey: "product_id",
+    as: "product"
+});
+
+//Relation NN Produit/Commande
+Product.belongsToMany(Order, {
+    as: "orders",
+    through: "product_order",
+    foreignKey: "product_id",
+    otherKey: "order_id"
+});
+
+Order.belongsToMany(Product, {
+    as: "products",
+    through: "product_order",
+    foreignKey: "order_id",
+    otherKey: "product_id"
+});
+
+//Relation 1N Produit/Avis
+Product.hasMany(Review, {
+    foreignKey: "product_id",
+    as: "reviews"
+});
+
+Review.belongsTo(Product, {
+    foreignKey: "product_id",
+    as: "product"
+});
+
+//Relation NN Produit/Allegène
+Product.belongsToMany(Allergen, {
+    as: "allergens",
+    through: "product_allergen",
+    foreignKey: "product_id",
+    otherKey: "allergen_id"
+});
+
+Allergen.belongsToMany(Product, {
+    as: "products",
+    through: "product_allergen",
+    foreignKey: "allergen_id",
+    otherKey: "product_id"
+});
+
+//Relation 1N Compte/Commande
+Account.hasMany(Order, {
+    foreignKey: "account_id",
+    as: "orders"
+});
+
+Account.belongsTo(Product, {
+    foreignKey: "account_id",
+    as: "account"
+});
+
+// Relation 1N Compte/Avis
+Account.hasMany(Review, {
+    foreignKey: "account_id",
+    as: "reviews"
+});
+
+Review.belongsTo(Product, {
+    foreignKey: "account_id",
+    as: "account"
+});
+
+// Relation NN Compte/Allergène
+Account.belongsToMany(Allergen, {
+    as: "allergens",
+    through: "account_allergen",
+    foreignKey: "account_id",
+    otherKey: "allergen_id"
+});
+
+Allergen.belongsToMany(Account, {
+    as: "products",
+    through: "account_allergen",
+    foreignKey: "allergen_id",
+    otherKey: "account_id"
+});
