386d26d8b7756575c217d76d5e45455d7615b161
YunxiBZ <zhangyunxi2012@gmail.com>
2021-03-03 11:33:02 +0100

    Add Scss to Component Footer with RWD

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,41 +7,43 @@ import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => (
   <div className="footer">
+    <div className="footer__top">
+      <FontAwesomeIcon icon={faArrowRight} />
+      <p className="footer__content">Copyright</p>
+    </div>
 
-    <FontAwesomeIcon icon={faArrowRight} />
-
-    <p className="footer__content">Copyright</p>
-
-    <Link
-      to="/RGPD"
-      className="footer__links"
-    >
-      RGPD
-    </Link>
-    <Link
-      to="/CGV"
-      className="footer__links"
-    >
-      CGV
-    </Link>
-    <Link
-      to="/Formulaire-de-contact"
-      className="footer__links"
-    >
-      Formulaire de contact
-    </Link>
-    <Link
-      to="/Mentions-legals"
-      className="footer__links"
-    >
-      Mentions légals
-    </Link>
-    <Link
-      to="/Groupe-de-dev"
-      className="footer__links"
-    >
-      Groupe de dev
-    </Link>
+    <nav className="footer__list">
+      <Link
+        to="/RGPD"
+        className="footer__links"
+      >
+        RGPD
+      </Link>
+      <Link
+        to="/CGV"
+        className="footer__links"
+      >
+        CGV
+      </Link>
+      <Link
+        to="/Formulaire-de-contact"
+        className="footer__links"
+      >
+        Formulaire de contact
+      </Link>
+      <Link
+        to="/Mentions-legals"
+        className="footer__links"
+      >
+        Mentions légals
+      </Link>
+      <Link
+        to="/Groupe-de-dev"
+        className="footer__links"
+      >
+        Groupe de dev
+      </Link>
+    </nav>
 
   </div>
 );
