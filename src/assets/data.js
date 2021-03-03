34a565b2be0a892d58a3eecb11479e801bcc8607
TheoB-png <theo.beloeil.pro@gmail.com>
2021-03-03 10:46:31 +0100

    fake data for home-page

---
+++ src/assets/data.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    slogan: 'Le pain qu\'il est bon',
+    history: 'Nous avons une histoire incroyable bla bla bla bla bla bla bla',
+    opening_time: {
+      monday: '8h-18h',
+      tuesday: '8h-18h',
+      wednesday: '8h-18h',
+      thursday: '8h-18h',
+      friday: '8h-18h',
+      saturday: '8h-18h',
+      sunday: '8h-18h',
+    },
+    hot_bread: {
+      first: '7h',
+      second: '12h',
+      third: '16',
+    },
+  },
+];
