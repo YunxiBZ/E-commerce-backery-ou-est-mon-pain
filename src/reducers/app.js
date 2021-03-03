a0d3694686dbc81480603051cd35f9eec751c831
YunxiBZ <zhangyunxi2012@gmail.com>
2021-03-03 16:34:28 +0100

    Fix conflic in reducers

--- src/reducers/app.js
+++ src/reducers/app.js
@@ -6,11 +6,8 @@ import data from 'src/assets/data';
 
 const initialState = {
   toggle: false,
-<<<<<<< HEAD
   toggleFooterlist: false,
-=======
   data,
->>>>>>> 3730c1c2d94d9983e890d262281a83f5bb548a17
 };
 
 export default (state = initialState, action = {}) => {
