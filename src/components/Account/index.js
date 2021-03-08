1ca1e9c4572fc449624d75b425469c9354c9880f
TheoB-png <theo.beloeil.pro@gmail.com>
2021-03-08 14:01:08 +0100

    styling component Account, add modify info Icons

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import './styles.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPen } from '@fortawesome/free-solid-svg-icons';
 
 const Account = () => {
   const ok = 'truc';
 
   return (
-    <div>Hello</div>
+    <div className="account">
+      <section className="account__userInterface">
+        <div className="account__info">
+          <p className="account__infoData">Prénom</p>
+          <FontAwesomeIcon
+            icon={faPen}
+          />
+        </div>
+        <div className="account__info">
+          <p className="account__infoData">Nom</p>
+          <FontAwesomeIcon
+            icon={faPen}
+          />
+        </div>
+        <div className="account__info">
+          <p className="account__infoData">téléphone</p>
+          <FontAwesomeIcon
+            icon={faPen}
+          />
+        </div>
+        <div className="account__info">
+          <p className="account__infoData">email</p>
+          <FontAwesomeIcon
+            icon={faPen}
+          />
+        </div>
+      </section>
+    </div>
   );
 };
 
