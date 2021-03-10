b2db332f91930cb2c0ebae8b885bc0e211d7074b
YunxiBZ <zhangyunxi2012@gmail.com>
2021-03-10 15:33:53 +0100

    Fix handle order with axios

--- src/middlewares/order.js
+++ src/middlewares/order.js
@@ -4,24 +4,22 @@ import { HANDLE_ORDER, orderSuccess, orderFailed } from 'src/actions/cart';
 const orderCart = (store) => (next) => (action) => {
   const { token } = store.getState().user.infos;
   console.log('middlewares token', token);
-  const config = {
-    headers: { Authorization: `Bearer ${token}` },
-  };
+  const { cart, totalPriceInCart } = store.getState().cartReducer;
+  const baseUrl = process.env.REACT_APP_BASE_URL;
+  console.log('cart', cart);
   switch (action.type) {
     case HANDLE_ORDER: {
-      const { cart, totalPriceInCart } = store.getState().cartReducer;
-      const baseUrl = process.env.REACT_APP_BASE_URL;
       const order = async () => {
         try {
-          const response = await axios({
-            url: `${baseUrl}/order`,
-            method: 'post',
-            data: {
-              reception_date: '2021-03-09',
-              products: cart,
-              total_price: totalPriceInCart,
+          const response = await axios.post(`${baseUrl}/order`, {
+            reception_date: '2021-03-09',
+            products: cart,
+            total_price: totalPriceInCart,
+          },
+          {
+            headers: {
+              authorization: `Bearer ${token}`,
             },
-            config,
           });
           console.log('order orderSuccess', response);
           store.dispatch(orderSuccess(response.date));
