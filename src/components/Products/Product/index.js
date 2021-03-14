bd6dfd1173b2783dd3ff4d8d27d972c359c50f17
TheoB-png <theo.beloeil.pro@gmail.com>
2021-03-14 19:56:22 +0100

    test animation on products

--- src/components/Products/Product/index.js
+++ src/components/Products/Product/index.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSpring, animated } from 'react-spring';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -16,60 +17,78 @@ const Product = ({
   onClickReduceQuantityBtn,
   onChangeQuantityInput,
   onClickAddToCartBtn,
-}) => (
-  <div className="product">
-    <h3 className="product__name">{title}</h3>
-    <Link to={`/product/${id}`}>
-      <img className="product__img" src={image} alt={`image_${title}`} />
-    </Link>
-    <div className="product__bottom">
-      <div className="product__quantity">
-        <FontAwesomeIcon
-          icon="arrow-alt-circle-down"
-          className="product__icon product__reduce-icon"
-          onClick={() => {
-            onClickReduceQuantityBtn(title);
-          }}
-        />
-        <input
-          className="product__quantity-input"
-          placeholder="Quantité"
-          type="text"
-          value={quantity}
-          name="quantity"
-          onChange={
+}) => {
+  // Gestion du hover sur un produit si on hover notre props hover
+  // Lorsque l'on quitte un produit de la souris hover est à false
+  const [hover, setHover] = useState(false);
+  const onHoverProduct = () => setHover(true);
+  const onMouseOutProduct = () => setHover(false);
+
+  // Animation
+  // On définit 3 couleurs que l'on va appliquer au bg-color de notre div
+  // en fonction du state du dessus.
+  const darkGrey = '#303a44';
+  const normalGrey = '#56666e';
+  const lightGrey = '#caccc9';
+  const [props, set] = useSpring(() => ({ backgroundColor: darkGrey }));
+  //
+  set({ backgroundColor: hover ? darkGrey : lightGrey });
+
+  return (
+    <animated.div style={props} className="product" props={hover} onMouseEnter={onHoverProduct} onMouseLeave={onMouseOutProduct}>
+      <h3 className="product__name">{title}</h3>
+      <Link to={`/product/${id}`}>
+        <img className="product__img" src={image} alt={`image_${title}`} />
+      </Link>
+      <div className="product__bottom">
+        <div className="product__quantity">
+          <FontAwesomeIcon
+            icon="arrow-alt-circle-down"
+            className="product__icon product__reduce-icon"
+            onClick={() => {
+              onClickReduceQuantityBtn(title);
+            }}
+          />
+          <input
+            className="product__quantity-input"
+            placeholder="Quantité"
+            type="text"
+            value={quantity}
+            name="quantity"
+            onChange={
             (evt) => {
               const quantityInput = evt.target.value;
               onChangeQuantityInput(quantityInput, title);
             }
           }
-        />
-        <FontAwesomeIcon
-          icon="arrow-alt-circle-up"
-          className="product__icon product__add-icon"
-          onClick={() => onClickAddQuantityBtn(title)}
-        />
+          />
+          <FontAwesomeIcon
+            icon="arrow-alt-circle-up"
+            className="product__icon product__add-icon"
+            onClick={() => onClickAddQuantityBtn(title)}
+          />
+        </div>
+
+        <p className="product__price">Prix: {totalPrice}€ </p>
+
       </div>
-
-      <p className="product__price">Prix: {totalPrice}€ </p>
-
-    </div>
-    <Button
-      className="product__add-to-cart-btn"
-      value="Ajouter au panier"
-      addInCart={() => {
-        onClickAddToCartBtn(
-          id,
-          title,
-          price,
-          totalPrice,
-          image,
-          quantity,
-        );
-      }}
-    />
-  </div>
-);
+      <Button
+        className="product__add-to-cart-btn"
+        value="Ajouter au panier"
+        addInCart={() => {
+          onClickAddToCartBtn(
+            id,
+            title,
+            price,
+            totalPrice,
+            image,
+            quantity,
+          );
+        }}
+      />
+    </animated.div>
+  );
+};
 
 Product.propTypes = {
   title: PropTypes.string.isRequired,
