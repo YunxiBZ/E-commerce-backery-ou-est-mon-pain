81b10f8f792f5a03c9408630df857ab7add96e8b
YunxiBZ <zhangyunxi2012@gmail.com>
2021-03-16 10:04:39 +0100

    Modify scss of RGPD page

--- src/components/RGPD/index.js
+++ src/components/RGPD/index.js
@@ -3,10 +3,10 @@ import './styles.scss';
 
 function RGPD() {
   return (
-    <div className="Container">
-      <h1 className="RGPDTitle">Politique de confidentialité</h1>
-      <h2 className="RGPDSubtitle">Protection des données</h2>
-      <p>
+    <div className="rgpd">
+      <h1 className="rgpd__title">Politique de confidentialité</h1>
+      <h2 className="rgpd__subtitle">Protection des données</h2>
+      <p className="rgpd__content">
         Le site ou-est-mon-pain.surge.sh est soucieux de la protection des
         données personnelles. Le site ou-est-mon-pain.surge.sh s’engage à
         assurer le meilleur niveau de protection à vos données personnelles en
@@ -21,8 +21,8 @@ function RGPD() {
         vos commandes en magasins sont traitées par la société : Où-est-mon-pain
         26 rue des pissenlits, 95100, Argenteuil.
       </p>
-      <h3 className="RGPDSubsubtitle">1.Traitement des données personnelles</h3>
-      <p>
+      <h3 className="rgpd__subtitle">1.Traitement des données personnelles</h3>
+      <p className="rgpd__content">
         Le site ou-est-mon-pain.surge.sh est amené à vous demander des
         informations personnelles dans le but de mieux vous satisfaire en
         répondant de manière personnalisée à vos attentes. Certaines sont
@@ -38,24 +38,24 @@ function RGPD() {
         rubrique » aide » de votre navigateur Internet pour paramétrer ces
         fonctions).
       </p>
-      <h3 className="RGPDSubsubtitle">2.Relation commerciale</h3>
-      <p>
+      <h3 className="rgpd__subtitle">2.Relation commerciale</h3>
+      <p className="rgpd__content">
         Dans le cadre de notre relation commerciale, nous sommes amenés à vous
         contacter par e-mail pour plusieurs raisons : confirmation de votre
         commande, envoi de votre commande.
       </p>
-      <h3 className="RGPDSubsubtitle">3. Accès à vos données personnelles</h3>
-      <p>
+      <h3 className="rgpd__subtitle">3. Accès à vos données personnelles</h3>
+      <p className="rgpd__content">
         Conformément à l’article 34 de la loi 78-17 du 6 janvier 1978 dite loi »
         Informatiques et Libertés » vous disposez d’un droit d’accès, de
         modification, de rectification et de suppression de vos données
         personnelles. Vous pouvez exercer ce droit en envoyant un e-mail à
         l’adresse suivante ou-est-mon-pain@gmail.com .
       </p>
-      <h3 className="RGPDSubsubtitle">
+      <h3 className="rgpd__subtitle">
         4. Transmission de vos données personnelles
       </h3>
-      <p>
+      <p className="rgpd__content">
         Certaines informations que vous nous communiquez peuvent être transmises
         à nos fournisseurs pour le traitement de votre commande. Ces
         informations seront considérées par le site ou-est-mon-pain.surge.sh
